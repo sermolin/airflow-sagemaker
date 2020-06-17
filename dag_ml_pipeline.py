145e65052dfe68c5ca956e37d4c7beecbcbe3fc0
Je Park <jpark@Jes-MacBook-Pro-2.local>
2020-06-17 16:00:54 -0400

    moved and deleted files

--- dag_ml_pipeline.py
+++ dag_ml_pipeline.py
@@ -170,6 +170,9 @@ cleanup_task = DummyOperator(
     task_id='cleaning_up',
     dag=dag)
 
-init.set_downstream(sm_proc_job_task)
-sm_proc_job_task.set_downstream(train_model_task)
-train_model_task.set_downstream(cleanup_task)
+# init.set_downstream(sm_proc_job_task)
+# sm_proc_job_task.set_downstream(train_model_task)
+# train_model_task.set_downstream(cleanup_task)
+
+init.set_downstream(train_model_task)
+train_model_task,set_downstream(cleanup_task)
