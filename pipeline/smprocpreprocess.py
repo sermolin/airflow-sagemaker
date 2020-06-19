d648d82c62627309830d8cd35d55ef0fc6db0477
Park <jeprk@38f9d3580df3.ant.amazon.com>
2020-06-19 14:32:58 -0400

    smproces

--- pipeline/smprocpreprocess.py
+++ pipeline/smprocpreprocess.py
@@ -86,6 +86,8 @@ def main():
             args['s3_output_bucket'], args['s3_output_key_prefix'], 'train', 'part-00000')
         s3.head_object(Bucket=args['s3_output_bucket'], Key=key)
         s3.delete_object(Bucket=args['s3_output_bucket'], Key=key)
+    except botocore.exceptions.ClientError as e:
+        pass
     else:
         pass
     train_lines.saveAsTextFile(
