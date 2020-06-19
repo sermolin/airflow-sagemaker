47409f9b609251e94f96d2e09230ed53988d57a1
Je Park <jpark@Jes-MacBook-Pro-2.local>
2020-06-19 01:26:17 -0400

    last

--- pipeline/inference_pipeline_ep.py
+++ pipeline/inference_pipeline_ep.py
@@ -8,33 +8,33 @@ import sys
 import schema_utils
 from sagemaker.amazon.amazon_estimator import get_image_uri
 
-sm = boto3.client('sagemaker', region_name='us-west-2')
+sm = boto3.client("sagemaker", region_name="us-west-2")
 
 def inference_pipeline_ep(role, sess, spark_model_uri, timestamp, **context):
     s3_sparkml_data_uri = spark_model_uri
-    s3_xgboost_model = sm.list_training_jobs(MaxResults=1, StatusEquals='Completed', SortBy='CreationTime', NameContains='training-job-', SortOrder='Descending')['TrainingJobSummaries'][0]['TrainingJobName']
+    s3_xgboost_model = sm.list_training_jobs(MaxResults=1, StatusEquals="Completed", SortBy="CreationTime", NameContains="training-job-", SortOrder="Descending")["TrainingJobSummaries"][0]["TrainingJobName"]
 
-    s3_xgboost_model_uri = 's3://airflow-sagemaker-jeprk/sagemaker/spark-preprocess-demo/model/xgboost/' + s3_xgboost_model + '/output/model.tar.gz'
+    s3_xgboost_model_uri = "s3://airflow-sagemaker-jeprk/sagemaker/spark-preprocess-demo/model/xgboost/" + s3_xgboost_model + "/output/model.tar.gz"
 
     xgb_container = get_image_uri(
-        sess.region_name, 'xgboost', repo_version="0.90-1")
+        sess.region_name, "xgboost", repo_version="0.90-1")
 
     schema_json = schema_utils.abalone_schema()
 
     sparkml_model = SparkMLModel(model_data=s3_sparkml_data_uri,  role=role, sagemaker_session=sagemaker.session.Session(
-        sess), env={'SAGEMAKER_SPARKML_SCHEMA': schema_json})
+        sess), env={"SAGEMAKER_SPARKML_SCHEMA": schema_json})
 
     xgb_model = Model(model_data=s3_xgboost_model_uri, role=role,
                       sagemaker_session=sagemaker.session.Session(sess), image=xgb_container)
 
-    pipeline_model_name = 'inference-pipeline-spark-xgboost-' + timestamp
+    pipeline_model_name = "inference-pipeline-spark-xgboost-" + timestamp
 
     sm_model = PipelineModel(name=pipeline_model_name,
                              role=role,
                              sagemaker_session=sagemaker.session.Session(sess),
                              models=[sparkml_model, xgb_model])
 
-    endpoint_name = 'inference-pipeline-endpoint-' + timestamp
+    endpoint_name = "inference-pipeline-endpoint-" + timestamp
     
     sm_model.deploy(initial_instance_count=1,
-                    instance_type='ml.c4.xlarge', endpoint_name=endpoint_name)
+                    instance_type="ml.c4.xlarge", endpoint_name=endpoint_name)
--- pipeline/sm_proc_job.py
+++ pipeline/sm_proc_job.py
@@ -8,24 +8,24 @@ import sys
 
 def sm_proc_job(role, sess, timestamp, **context):
 
-    bucket = 'airflow-sagemaker-jeprk'
+    bucket = "airflow-sagemaker-jeprk"
 
-    prefix = 'sagemaker/spark-preprocess-demo/'
-    input_preprocessed_prefix = prefix + '/input/preprocessed/'+timestamp+'abalone'
-    model_prefix = prefix + 'model/spark/'+timestamp
+    prefix = "sagemaker/spark-preprocess-demo/"
+    input_preprocessed_prefix = prefix + "/input/preprocessed/"+timestamp+"abalone"
+    model_prefix = prefix + "model/spark/"+timestamp
 
-    spark_repository_uri = '885332847160.dkr.ecr.us-west-2.amazonaws.com/sagemaker-spark'
+    spark_repository_uri = "885332847160.dkr.ecr.us-west-2.amazonaws.com/sagemaker-spark"
 
-    spark_processor = ScriptProcessor(base_job_name='spark-preprocessor',
+    spark_processor = ScriptProcessor(base_job_name="spark-preprocessor",
                                       image_uri=spark_repository_uri,
-                                      command=['/opt/program/submit'],
+                                      command=["/opt/program/submit"],
                                       role=role,
                                       sagemaker_session=sagemaker.Session(
                                           sess),
                                       instance_count=2,
-                                      instance_type='ml.r5.xlarge',
+                                      instance_type="ml.r5.xlarge",
                                       max_runtime_in_seconds=1200,
-                                      env={'mode': 'python'})
+                                      env={"mode": "python"})
 
-    spark_processor.run(code='s3://airflow-sagemaker-jeprk/code/smprocpreprocess.py', arguments=['s3_input_bucket', bucket, 's3_input_key_prefix', input_prefix,
-                                                                                                 's3_output_bucket', bucket, 's3_output_key_prefix', input_preprocessed_prefix, 's3_model_bucket', bucket, 's3_model_prefix', model_prefix], logs=True)
+    spark_processor.run(code="s3://airflow-sagemaker-jeprk/code/smprocpreprocess.py", arguments=["s3_input_bucket", bucket, "s3_input_key_prefix", input_prefix,
+                                                                                                 "s3_output_bucket", bucket, "s3_output_key_prefix", input_preprocessed_prefix, "s3_model_bucket", bucket, "s3_model_prefix", model_prefix], logs=True)
