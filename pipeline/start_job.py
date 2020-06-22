4b9926b1f2f6d0beed2729975418e5d43a68e03a
Park <jeprk@38f9d3580df3.ant.amazon.com>
2020-06-22 13:22:43 -0400

    gogogo

--- pipeline/start_job.py
+++ pipeline/start_job.py
@@ -12,8 +12,8 @@ def start(bucket):
     timestamp_prefix = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     Variable.set("timestamp", timestamp_prefix)
     print(os.getcwd())
-    s3.upload_file(
-        Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Filename='./abalone.csv')
-    s3.upload_file(Bucket=bucket, Key='code/smprocpreprocess.py',
-                   Filename='./smprocpreprocess.py')
+    # s3.upload_file(
+    #     Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Filename='./abalone.csv')
+    # s3.upload_file(Bucket=bucket, Key='code/smprocpreprocess.py',
+    #                Filename='./smprocpreprocess.py')
     time.sleep(10)
