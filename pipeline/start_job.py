b46be7e928bb11d33b9d3a708572cc0f362beed1
Park <jeprk@38f9d3580df3.ant.amazon.com>
2020-06-22 13:29:23 -0400

    /tmp

--- pipeline/start_job.py
+++ pipeline/start_job.py
@@ -11,7 +11,7 @@ s3 = boto3.client('s3')
 def start(bucket):
     timestamp_prefix = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     Variable.set("timestamp", timestamp_prefix)
-    print(os.listdir('/'))
+    print(os.listdir('/tmp'))
     # s3.upload_file(
     #     Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Filename='./abalone.csv')
     # s3.upload_file(Bucket=bucket, Key='code/smprocpreprocess.py',
