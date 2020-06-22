2c3566631986fc468b6ba15869e99b8624dc5a2a
Park <jeprk@38f9d3580df3.ant.amazon.com>
2020-06-22 13:53:02 -0400

    please

--- pipeline/start_job.py
+++ pipeline/start_job.py
@@ -1,16 +1,29 @@
 from airflow.models import Variable
 import time
 from time import gmtime, strftime
+import os
 import boto3
 
 s3 = boto3.client('s3')
 
 
+def find_files(file_name):
+    command = ['locate', file_name]
+
+    output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0]
+    output = output.decode()
+
+    search_results = output.split('\n')
+
+    return search_results
+
+
 def start(bucket):
     timestamp_prefix = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     Variable.set("timestamp", timestamp_prefix)
-    s3.put_object(
-        Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Body='abalone.csv')
-    s3.put_object(Bucket=bucket, Key='code/smprocpreprocess.py',
-                  Body='smprocpreprocess.py')
+    find_files('smprocpreprocess.py')
+    # s3.put_object(
+    #     Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Body=abalone.csv)
+    # s3.put_object(Bucket=bucket, Key='code/smprocpreprocess.py',
+    #               Body=smprocpreprocess.py)
     time.sleep(10)
