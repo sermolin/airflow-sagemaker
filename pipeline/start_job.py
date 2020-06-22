79887ec63927b2990736cbb1f22a443827e7d522
Park <jeprk@38f9d3580df3.ant.amazon.com>
2020-06-22 13:20:47 -0400

    gogo

--- pipeline/start_job.py
+++ pipeline/start_job.py
@@ -2,6 +2,7 @@ from airflow.models import Variable
 import time
 from time import gmtime, strftime
 import boto3
+import os
 import config as cfg
 
 s3 = boto3.client('s3')
@@ -10,6 +11,7 @@ s3 = boto3.client('s3')
 def start(bucket):
     timestamp_prefix = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     Variable.set("timestamp", timestamp_prefix)
+    print(os.getcwd())
     s3.upload_file(
         Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Filename='./abalone.csv')
     s3.upload_file(Bucket=bucket, Key='code/smprocpreprocess.py',
