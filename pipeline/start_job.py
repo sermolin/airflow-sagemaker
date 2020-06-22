2c97fdcfc3b64af19c933a182964c4461dd9e67c
Park <jeprk@38f9d3580df3.ant.amazon.com>
2020-06-22 13:35:18 -0400

    put objct

--- pipeline/start_job.py
+++ pipeline/start_job.py
@@ -11,9 +11,8 @@ s3 = boto3.client('s3')
 def start(bucket):
     timestamp_prefix = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     Variable.set("timestamp", timestamp_prefix)
-    print(os.listdir('~/airflow'))
-    # s3.upload_file(
-    #     Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Filename='./abalone.csv')
-    # s3.upload_file(Bucket=bucket, Key='code/smprocpreprocess.py',
-    #                Filename='./smprocpreprocess.py')
+    s3.put_object(
+        Bucket=bucket, Key='sagemaker/spark-preprocess/inputs/raw/abalone/abalone.csv', Body='/abalone.csv')
+    s3.put_object(Bucket=bucket, Key='code/smprocpreprocess.py',
+                  Body='/smprocpreprocess.py')
     time.sleep(10)
