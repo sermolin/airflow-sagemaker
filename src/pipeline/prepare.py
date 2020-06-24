4aecca5fe5832df7352a81eaed9d728fd6f6efc5
Park <jeprk@38f9d3580df3.ant.amazon.com>
2020-06-24 15:43:11 -0400

    new prepare.py script

--- src/pipeline/prepare.py
+++ src/pipeline/prepare.py
@@ -3,22 +3,10 @@ from time import gmtime, strftime
 import boto3
 
 
-def set_timestamp():
+def start(bucket, keys, file_paths):
     timestamp_prefix = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     Variable.set("timestamp", timestamp_prefix)
 
-
-def upload_to_s3(bucket, keys, file_paths):
-    s3 = boto3.client('s3')
-    input_key = keys[0]
-    input_file = file_paths[0]
-    preproc_key = keys[1]
-    preproc_file = file_paths[1]
-    s3.upload_file(Filename=input_file, Bucket=bucket, Key=input_key)
-    s3.upload_file(Filename=preproc_file, Bucket=bucket, Key=preproc_key)
-
-
-def start(bucket, keys, file_paths):
     s3 = boto3.client('s3')
     input_key = keys[0]
     input_file = file_paths[0]
